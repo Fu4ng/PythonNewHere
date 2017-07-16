9707910098fcb54153b5ddf8f3ec6ffe654ccb1d
Fu4ng <903838834@.com>
2017-07-16 22:26:09 +0800

    希望以后能找到一个好公司把，不说那么多，提升自己的实力把！！

---
+++ L03.py
@@ -0,0 +1,26 @@
+# --------------------------------------#
+#
+# Created by Fu4ng on 2017/7/16.
+#
+# --------------------------------------#
+
+# #模版字符串
+# from  string import Template
+# s = Template ("$x,Python $x!")
+# print(s.substitute(x = 'wow'))
+# #替换字段是单词的一部分，那么参数名就必须用括号扩起来，从而指明结尾
+# s = Template("It's ${x}thon")
+# print(s.substitute(x = 'Py'))
+
+# format1 = 'hello,%s,%s nough for ya?'
+# values = ('world','hot')
+# print(format1 % values)
+
+# from math import pi
+# print('%10f' % pi)
+# print('%10.2f' % pi)
+# print('%010.2f' % pi)
+# print('%-10.2f' % pi)
+# x = 'abcde'
+# print('%.2s' %x)
+# #关于字符串对齐的方法：http://jingyan.baidu.com/article/77b8dc7fe1371b6174eab691.html
