44c4abf67b8507593ee5480d2b71b08b13f4a22a
Fu4ng <903838834@.com>
2017-07-17 12:57:26 +0800

    translate的3.x版本的用法不一样了

--- L03.py
+++ L03.py
@@ -18,3 +18,40 @@
 # x = 'abcde'
 # print('%.2s' %x)
 # #关于字符串对齐的方法：http://jingyan.baidu.com/article/77b8dc7fe1371b6174eab691.html
+
+# # 字符串方法
+# #find
+# x='deacddef'
+# y = x.find('de')
+# print(y)
+# z = x.find('de',2,5)  #设置起始点和结束点参数
+# print(z)
+
+# #join和split
+#
+# s = ['1','2','3']
+# q ='+'
+# print(q.join(s))
+# m =(' ','usr','bin','env')
+# print('C:'+'/'.join(m))
+# print(q.join(s).split('+'))
+# print(('C:'+'/'.join(m)).split('/'))
+
+# #replace
+# x = 'This is a test'
+# print(x.replace('is','eez'))
+
+# #strip
+# x = '         去除两侧空格             '
+# print(x.strip())
+# x= 5*'+'+x+5*'+'
+# print(x)
+# print((x.strip('+')).strip())
+
+# translate
+x = '123456'
+tab = x.maketrans('1235', 'abce', '6')
+print(x.translate(tab))
+
+
+# translate
